91223e96a29ee8d40c8d8b547f64fc597d4bab2a
Daniel Hazelbaker <daniel@blueboxmoon.com>
2019-01-10 17:15:26 -0800

    - Moved HereRestApi namespace under new External namespace, to indicate external services.

--- RockSweeper/External/HereRestApi.cs
+++ RockSweeper/External/HereRestApi.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace RockSweeper.HereRestApi
+namespace RockSweeper.External.HereRestApi
 {
     public class MetaInfo
     {
--- RockSweeper/SweeperController.partial.cs
+++ RockSweeper/SweeperController.partial.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Threading;
 using RockSweeper.Utility;
+using HereRestApi = RockSweeper.External.HereRestApi;
 
 namespace RockSweeper
 {
