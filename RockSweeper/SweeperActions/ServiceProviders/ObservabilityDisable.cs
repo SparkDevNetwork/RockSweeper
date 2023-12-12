1640a3883e4c17d6ef1feb03ad97c881e297b989
Daniel Hazelbaker <daniel@triumph.tech>
2023-12-12 15:13:06 -0700

    + Added actions to disable or reset Observability settings.

---
+++ RockSweeper/SweeperActions/ServiceProviders/ObservabilityDisable.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+using RockSweeper.Attributes;
+
+namespace RockSweeper.SweeperActions.ServiceProviders
+{
+    /// <summary>
+    /// Updates the Rock configuration to that the observability feature is disabled.
+    /// </summary>
+    [ActionId( "5e27c631-3681-4d1b-b50c-0e5f3898e541" )]
+    [Title( "Observability (Disable)" )]
+    [Description( "Updates the Rock configuration to that the observability feature is disabled." )]
+    [Category( "Service Providers" )]
+    [DefaultValue( true )]
+    [ConflictsWithAction( typeof( ObservabilityReset ) )]
+    public class ObservabilityDisable : SweeperAction
+    {
+        public override async Task ExecuteAsync()
+        {
+            await Sweeper.SqlCommandAsync( "DELETE FROM [Attribute] WHERE [EntityTypeQualifierColumn] = 'SystemSetting' AND [Key] = 'core_ObservabilityEnabled'" );
+        }
+    }
+}
---
+++ RockSweeper/SweeperActions/ServiceProviders/ObservabilityReset.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+using RockSweeper.Attributes;
+
+namespace RockSweeper.SweeperActions.ServiceProviders
+{
+    /// <summary>
+    /// Resets all observability settings to system default values.
+    /// </summary>
+    [ActionId( "12956c26-2965-4c40-9cfd-28c542357e55" )]
+    [Title( "Observability (Reset)" )]
+    [Description( "Resets all observability settings to system default values." )]
+    [Category( "Service Providers" )]
+    [ConflictsWithAction( typeof( ObservabilityDisable ) )]
+    public class ObservabilityReset : SweeperAction
+    {
+        public override async Task ExecuteAsync()
+        {
+            await Sweeper.SqlCommandAsync( "DELETE FROM [Attribute] WHERE [EntityTypeQualifierColumn] = 'SystemSetting' AND [Key] LIKE 'core_Observability%'" );
+        }
+    }
+}
